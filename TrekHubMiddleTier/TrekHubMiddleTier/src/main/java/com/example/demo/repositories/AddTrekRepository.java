388a114be079bd1217847126678fdf2699cc66ed
69287_mayur <torawanemayur121@gmail.com>
2023-08-23 08:41:17 +0530

    add

---
+++ TrekHubMiddleTier/TrekHubMiddleTier/src/main/java/com/example/demo/repositories/AddTrekRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.entities.AddTrek;
+
+public interface AddTrekRepository extends JpaRepository<AddTrek, Integer> 
+{
+//	@Modifying
+//	@Query("update AddTrek t set t.trekimage=:data where t.trek_id=:id")
+//	public int uploadPhoto(int id, byte [] data);
+
+}
